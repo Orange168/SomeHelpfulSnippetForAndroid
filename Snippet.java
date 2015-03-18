0726b23d1432e8594e369f75f17a155b0e9eba39
Zhi Lin <zhilinchn@gmail.com>
2015-03-18 16:12:33 +0800

    Create Snippet.java

---
+++ Snippet.java
@@ -0,0 +1,20 @@
+//android.support.v7.widget
+//API 20 UI com.example.android.actionbarcompat.listpopupmenu
+ PopupMenu popup = new PopupMenu(getActivity(), view);
+
+        // Inflate our menu resource into the PopupMenu's Menu
+        popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
+
+        // Set a listener so we are notified if a menu item is clicked
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.menu_remove:
+                        // Remove the item from the adapter
+                        adapter.remove(item);
+                        return true;
+                }
+                return false;
+            }
+        });
