65306edf6900553a4198b83cc87edd84061d2a78
Zhi Lin <zhilinchn@gmail.com>
2015-03-18 15:47:08 +0800

    Create ActionBar.java

---
+++ ActionBar.java
@@ -0,0 +1,17 @@
+//actionbarcompat 
+//come from Android 20ui com.example.android.actionbarcompat.basic
+@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate our menu from the resources by using the menu inflater.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // It is also possible add items here. Use a generated id from
+        // resources (ids.xml) to ensure that all menu ids are distinct.
+        MenuItem locationItem = menu.add(0, R.id.menu_location, 0, R.string.menu_location);
+        locationItem.setIcon(R.drawable.ic_action_location);
+
+        // Need to use MenuItemCompat methods to call any action item related methods
+        MenuItemCompat.setShowAsAction(locationItem, MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+        return true;
+    }
