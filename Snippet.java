aead7a184768edd0004a1cdf649ee706376b18d1
Zhi Lin <zhilinchn@gmail.com>
2015-03-19 11:24:00 +0800

    add TextSwitcher snippet

--- Snippet.java
+++ Snippet.java
@@ -18,3 +18,58 @@
                 return false;
             }
         });
+
+//API 20 UI com.example.android.textswitcher
+// TextSwitcher Sample 
+//demoImage: TextSwitcher.png
+   // 1. find id and Set the factory used to create TextViews to switch between.
+        mSwitcher.setFactory(mFactory);
+
+        /*
+         * Set the in and out animations. Using the fade_in/out animations
+         * provided by the framework.
+         */
+        Animation in = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_in_left);
+        Animation out = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_out_right);
+        mSwitcher.setInAnimation(in);
+        mSwitcher.setOutAnimation(out);
+
+
+        /*
+         * Setup the 'next' button. The counter is incremented when clicked and
+         * the new value is displayed in the TextSwitcher. The change of text is
+         * automatically animated using the in/out animations set above.
+         */
+        Button nextButton = (Button) findViewById(R.id.button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mCounter++;
+
+                mSwitcher.setText(String.valueOf(mCounter));
+
+            }
+        });
+
+        // Set the initial text without an animation
+        mSwitcher.setCurrentText(String.valueOf(mCounter));
+        
+            /**
+     * The {@link android.widget.ViewSwitcher.ViewFactory} used to create {@link android.widget.TextView}s that the
+     * {@link android.widget.TextSwitcher} will switch between.
+     */
+    private ViewFactory mFactory = new ViewFactory() {
+
+        @Override
+        public View makeView() {
+
+            // Create a new TextView
+            TextView t = new TextView(MainActivity.this);
+            t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+            t.setTextAppearance(MainActivity.this, android.R.style.TextAppearance_Large);
+            return t;
+        }
+    };
