6e86e615d009aa626689e84c35eb55a095360c71
Austin L. Gable <zhilinchn@gmail.com>
2015-03-22 06:55:55 +0800

    Create Annotation.java

---
+++ Annotation.java
@@ -0,0 +1,13 @@
+// get Annotation methds from android.view.ViewDebug.java
+  methods = klass.getDeclaredMethods();
+  int count = methods.length;
+        for (int i = 0; i < count; i++) {
+            final Method method = methods[i];
+            if (method.getParameterTypes().length == 0 &&
+                    method.isAnnotationPresent(ExportedProperty.class) &&
+                    method.getReturnType() != Void.class) {
+                method.setAccessible(true);
+                foundMethods.add(method);
+                sAnnotations.put(method, method.getAnnotation(ExportedProperty.class));
+            }
+        }
