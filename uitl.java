56fcfbf04c4165b6af1e43292bc1abab622647a7
Edward·D·Lin <zhilinchn@gmail.com>
2015-06-10 11:28:05 +0800

    add getDefaultDisplay method

--- uitl.java
+++ uitl.java
@@ -10,4 +10,17 @@ public class DisplayUtil {
 		final float scale = context.getResources().getDisplayMetrics().density;                 
 		return (int)(pxValue / scale + 0.5f);         
 	} 
+	
+	 public static void  getDefaultDisplay(Activity activity){
+	 	
+	        DisplayMetrics metric = new DisplayMetrics();
+	        activity.getWindowManager().getDefaultDisplay().getMetrics(metric);
+	//       metric = activity.getResources().getDisplayMetrics() ;
+	        int screenWidth = metric.widthPixels;  // 屏幕宽度（像素）
+	        int screenHeight = metric.heightPixels;  // 屏幕高度（像素）
+	        float density = metric.density;  // 屏幕密度（0.75 / 1.0 / 1.5）
+	        int densityDpi = metric.densityDpi;  // 屏幕密度DPI（120 / 160 / 240）
+	        Log.e(TAG + "getDefaultDisplay", "screenWidth=" + screenWidth + "; screenHeight=" + screenHeight);
+	        Log.e(TAG + "getDefaultDisplay", "density=" + density + "; densityDpi=" + densityDpi);
+	  }
 }
