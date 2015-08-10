4a5e72dafcea09b9b46379551203bdc8458b7173
Edward·D·Lin <zhilinchn@gmail.com>
2015-08-10 11:19:18 +0800

    Update UncaughtExceptionHandler.java

--- UncaughtExceptionHandler.java
+++ UncaughtExceptionHandler.java
@@ -1,53 +1,76 @@
-/**
-* Application 
-*CrashHandler crashHandler = CrashHandler.getInstance();
-*crashHandler.init(getApplicationContext());
-**/
-public class CrashHandler implements UncaughtExceptionHandler {
 
-	private static final String TAG = CrashHandler.class.getSimpleName();
+package com.anl.phone.wxb.utils.exception;
 
-	private static CrashHandler instance; // 单例模式
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
 
-  private OnBeforeHandleExceptionListener mListener;
 
-	private Context context; // 程序Context对象
-	
-	private Thread.UncaughtExceptionHandler defalutHandler; // 系统默认的UncaughtException处理类
-	
-	private DateFormat formatter = new SimpleDateFormat(
-			"yyyy-MM-dd_HH-mm-ss.SSS", Locale.CHINA);
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+	/** TAG */
+	public static final String              TAG       = "CrashHandler";
+	/** 系统默认的UncaughtException处理类 */
+	private Thread.UncaughtExceptionHandler mDefaultHandler;
+	/** CrashHandler实例 */
+	private static CrashHandler             mCrashHandler;
+	/** 程序的Context对象 */
+	private Context                         mContext;
+	/** 用来存储设备信息和异常信息 */
+	private Map<String, String>             infos     = new HashMap<>();
+	/** 用于格式化日期,作为日志文件名的一部分 */
+	private DateFormat                      formatter = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss",Locale.CANADA);
+
+	/**
+	 * 私有构造函数
+	 */
 	private CrashHandler() {
 
 	}
 
 	/**
-	 * 获取CrashHandler实例
-	 * 
-	 * @return CrashHandler
+	 * 获取CrashHandler实例 ,单例模式
+	 *
+	 * @return
+	 * @since V1.0
 	 */
 	public static CrashHandler getInstance() {
-		if (instance == null) {
-			synchronized (CrashHandler.class) {
-				if (instance == null) {
-					instance = new CrashHandler();
-				}
-			}
-		}
-
-		return instance;
+		if (mCrashHandler == null)
+			mCrashHandler = new CrashHandler();
+		return mCrashHandler;
 	}
 
 	/**
-	 * 异常处理初始化
-	 * 
+	 * 初始化
+	 *
 	 * @param context
+	 * @since V1.0
 	 */
 	public void init(Context context) {
-		this.context = context;
+		mContext = context;
 		// 获取系统默认的UncaughtException处理器
-		defalutHandler = Thread.getDefaultUncaughtExceptionHandler();
+		mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
 		// 设置该CrashHandler为程序的默认处理器
 		Thread.setDefaultUncaughtExceptionHandler(this);
 	}
@@ -57,22 +80,14 @@ public class CrashHandler implements UncaughtExceptionHandler {
 	 */
 	@Override
 	public void uncaughtException(Thread thread, Throwable ex) {
-        //异常发生时，先给蓝牙服务端发送OK命令
-        if (mListener != null) {
-            mListener.onBeforeHandleException();
-        }
-
-		// 自定义错误处理
-		boolean res = handleException(ex);
-		if (!res && defalutHandler != null) {
+		if (!handleException(ex) && mDefaultHandler != null) {
 			// 如果用户没有处理则让系统默认的异常处理器来处理
-			defalutHandler.uncaughtException(thread, ex);
-
+			mDefaultHandler.uncaughtException(thread, ex);
 		} else {
 			try {
 				Thread.sleep(3000);
 			} catch (InterruptedException e) {
-				Log.e(TAG, "error : ", e);
+				Log.e(TAG, "uncaughtException() InterruptedException:" + e);
 			}
 			// 退出程序
 			android.os.Process.killProcess(android.os.Process.myPid());
@@ -82,34 +97,150 @@ public class CrashHandler implements UncaughtExceptionHandler {
 
 	/**
 	 * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
-	 * 
+	 *
 	 * @param ex
 	 * @return true:如果处理了该异常信息;否则返回false.
+	 * @since V1.0
 	 */
-	private boolean handleException(final Throwable ex) {
+	private boolean handleException(Throwable ex) {
 		if (ex == null) {
 			return false;
 		}
 
-		new Thread() {
+		// 收集设备参数信息
+		collectDeviceInfo(mContext);
 
+		// 使用Toast来显示异常信息
+		new Thread() {
 			@Override
 			public void run() {
 				Looper.prepare();
-
-				ex.printStackTrace();
-				String err = "[" + ex.getMessage() + "]";
-				Toast.makeText(context, "程序出现异常." + err, Toast.LENGTH_LONG)
-						.show();
-
+				Toast.makeText(mContext, "很抱歉,程序出现异常,即将退出.", Toast.LENGTH_SHORT).show();
 				Looper.loop();
 			}
-
 		}.start();
 
-		// 收集设备参数信息 \日志信息
-		String errInfo = collectDeviceInfo(context, ex);
 		// 保存日志文件
-		saveCrashInfo2File(errInfo);
-		return true;                                                                                                                }
+		saveCatchInfo2File(ex);
+		return true;
+	}
+
+	/**
+	 * 收集设备参数信息
+	 *
+	 * @param ctx
+	 * @since V1.0
+	 */
+	public void collectDeviceInfo(Context ctx) {
+		try {
+			PackageManager pm = ctx.getPackageManager();
+			PackageInfo pi = pm.getPackageInfo(ctx.getPackageName(), PackageManager.GET_ACTIVITIES);
+			if (pi != null) {
+				String versionName = pi.versionName == null ? "null" : pi.versionName;
+				String versionCode = pi.versionCode + "";
+				infos.put("versionName", versionName);
+				infos.put("versionCode", versionCode);
+			}
+		} catch (PackageManager.NameNotFoundException e) {
+			Log.e(TAG, "collectDeviceInfo() an error occurred when collect package info NameNotFoundException:", e);
+		}
+		Field[] fields = Build.class.getDeclaredFields();
+		for (Field field : fields) {
+			try {
+				field.setAccessible(true);
+				infos.put(field.getName(), field.get(null).toString());
+				Log.d(TAG, field.getName() + " : " + field.get(null));
+			} catch (Exception e) {
+				Log.e(TAG, "collectDeviceInfo() an error occurred when collect crash info Exception:", e);
+			}
+		}
+	}
+
+	/**
+	 * 保存错误信息到文件中
+	 *
+	 * @param ex
+	 * @return 返回文件名称,便于将文件传送到服务器
+	 */
+	private String saveCatchInfo2File(Throwable ex) {
+		StringBuilder sb = new StringBuilder();
+		for (Map.Entry<String, String> entry : infos.entrySet()) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+			sb.append(key).append("=").append(value).append("\n");
+		}
+
+		Writer writer = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(writer);
+		ex.printStackTrace(printWriter);
+		Throwable cause = ex.getCause();
+		while (cause != null) {
+			cause.printStackTrace(printWriter);
+			cause = cause.getCause();
+		}
+		printWriter.close();
+		String result = writer.toString();
+		sb.append(result);
+		try {
+			long timestamp = System.currentTimeMillis();
+			String time = formatter.format(new Date());
+			String fileName = "crash-" + time + "-" + timestamp + ".log";
+			if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+				String path = Environment.getExternalStorageState() + "/crash/";
+				File dir = new File(path);
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
+				FileOutputStream fos = new FileOutputStream(path + fileName);
+				fos.write(sb.toString().getBytes());
+				// 发送给开发人员
+				sendCrashLog2PM(path + fileName);
+				fos.close();
+			}
+			return fileName;
+		} catch (Exception e) {
+			Log.e(TAG, "saveCatchInfo2File() an error occured while writing file... Exception:", e);
+		}
+		return null;
+	}
+
+	/**
+	 * 将捕获的导致崩溃的错误信息发送给开发人员 目前只将log日志保存在sdcard 和输出到LogCat中，并未发送给后台。
+	 *
+	 * @param fileName
+	 * @since V1.0
+	 */
+	private void sendCrashLog2PM(String fileName) {
+		if (!new File(fileName).exists()) {
+			Log.e(TAG, "sendCrashLog2PM() 日志文件不存在");
+			return;
+		}
+		FileInputStream fis = null;
+		BufferedReader reader = null;
+		String s ;
+		try {
+			fis = new FileInputStream(fileName);
+			reader = new BufferedReader(new InputStreamReader(fis, "GBK"));
+			while (true) {
+				s = reader.readLine();
+				if (s == null)
+					break;
+				// 由于目前尚未确定以何种方式发送，所以先打出log日志。
+				Log.e(TAG, s);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally { // 关闭流
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
