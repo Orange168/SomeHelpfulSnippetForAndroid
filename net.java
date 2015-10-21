a174b7d51db1cb2fbba274aba426927e0a27622d
Edward·D·Lin <zhilinchn@gmail.com>
2015-10-21 16:40:55 +0800

    Create net.java

---
+++ net.java
@@ -0,0 +1,13 @@
+   public static boolean hasStorage2Download(@NonNull final Context context,String sUrl) {
+        try {
+            URL url = new URL(sUrl);
+            HttpURLConnection cnn = (HttpURLConnection) url.openConnection();
+            int fileLength =  cnn.getContentLength()/*19.2 MiB  return 20075405byte*/;
+
+            Toast.makeText(context,"File Size ==>>" + fileLength,Toast.LENGTH_LONG).show();
+            return fileLength < getAvailableStorage() ;
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
