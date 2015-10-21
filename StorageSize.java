f955eb82e3a7c00a0b0e5ec64e7a3f3b485f7efd
Edward·D·Lin <zhilinchn@gmail.com>
2015-10-21 17:44:20 +0800

    Update StorageSize.java

--- StorageSize.java
+++ StorageSize.java
@@ -1,51 +1,70 @@
-/**
-	 * 获得SD卡总大小
-	 * 
-	 * @return
-	 */
-	private String getSDTotalSize() {
-		File path = Environment.getExternalStorageDirectory();
-		StatFs stat = new StatFs(path.getPath());
-		long blockSize = stat.getBlockSize();
-		long totalBlocks = stat.getBlockCount();
-		return Formatter.formatFileSize(MainActivity.this, blockSize * totalBlocks);
-	}
+//    1. 判断 SDCard 是否存在,并且是否具有可读写权限
+    /**
+     * 外部存储是否可用 (存在且具有读写权限)
+     * @return
+     */
+     public static boolean isExternalStorageAvailable() {
+        return Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED);
+    }
 
-	/**
-	 * 获得sd卡剩余容量，即可用大小
-	 * 
-	 * @return
-	 */
-	private String getSDAvailableSize() {
-		File path = Environment.getExternalStorageDirectory();
-		StatFs stat = new StatFs(path.getPath());
-		long blockSize = stat.getBlockSize();
-		long availableBlocks = stat.getAvailableBlocks();
-		return Formatter.formatFileSize(MainActivity.this, blockSize * availableBlocks);
-	}
+//    2.获取手机系统可用空间大小
+    /**
+     * 获取手机内部可用空间大小
+     * @return
+     */
+    static public long getAvailableInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();
+        long availableBlocks = stat.getAvailableBlocks();
+        return availableBlocks * blockSize;
+    }
 
-	/**
-	 * 获得机身内存总大小
-	 * 
-	 * @return
-	 */
-	private String getRomTotalSize() {
-		File path = Environment.getDataDirectory();
-		StatFs stat = new StatFs(path.getPath());
-		long blockSize = stat.getBlockSize();
-		long totalBlocks = stat.getBlockCount();
-		return Formatter.formatFileSize(MainActivity.this, blockSize * totalBlocks);
-	}
 
-	/**
-	 * 获得机身可用内存
-	 * 
-	 * @return
-	 */
-	private String getRomAvailableSize() {
-		File path = Environment.getDataDirectory();
-		StatFs stat = new StatFs(path.getPath());
-		long blockSize = stat.getBlockSize();
-		long availableBlocks = stat.getAvailableBlocks();
-		return Formatter.formatFileSize(MainActivity.this, blockSize * availableBlocks);
-	}
+
+
+//    3.获取手机内部总空间大小
+    /**
+     * 获取手机内部空间大小
+     * @return
+     */
+    static public long getTotalInternalMemorySize() {
+        File path = Environment.getDataDirectory();//Gets the Android data directory
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();      //每个block 占字节数
+        long totalBlocks = stat.getBlockCount();   //block总数
+        return totalBlocks * blockSize;
+    }
+//    4.获取手机外部可用空间大小、获取手机外部总空间大小
+    /**
+     * 获取手机外部可用空间大小
+     * @return
+     */
+    static public long getAvailableExternalMemorySize() {
+        if (isExternalStorageAvailable()) {
+            File path = Environment.getExternalStorageDirectory();//获取SDCard根目录
+            StatFs stat = new StatFs(path.getPath());
+            long blockSize = stat.getBlockSize();
+            long availableBlocks = stat.getAvailableBlocks();
+            return availableBlocks * blockSize;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * 获取手机外部总空间大小
+     * @return
+     */
+    static public long getTotalExternalMemorySize() {
+        if (isExternalStorageAvailable()) {
+            File path = Environment.getExternalStorageDirectory(); //获取SDCard根目录
+            StatFs stat = new StatFs(path.getPath());
+            long blockSize = stat.getBlockSize();
+            long totalBlocks = stat.getBlockCount();
+            return totalBlocks * blockSize;
+        } else {
+            return -1;
+        }
+    }
