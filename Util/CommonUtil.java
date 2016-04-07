695064af308a5863a6da109a508d74fb7dfa6260
Edward·D·Lin <zhilinchn@gmail.com>
2016-04-07 16:11:01 +0800

    add SplitBitmap handler

--- Util/CommonUtil.java
+++ Util/CommonUtil.java
@@ -27,5 +27,34 @@ public class CommonUtils {
             }
         }
     }
+    
+    /**
+     * @param rawBitmap 原来的Bitmap
+     * @param row       切成几行
+     * @param column    切成几列
+     * @return
+     */
+    public static ArrayList<Bitmap> splitBitmap(Bitmap rawBitmap, int row, int column) {
+        ArrayList<Bitmap> mSplitList = new ArrayList<Bitmap>(row * column);
+        int rawBitmapWidth = rawBitmap.getWidth();
+        int rawBitmapHeight = rawBitmap.getHeight();
+
+        System.out.println("rawBitmapWidth=" + rawBitmapWidth + ",rawBitmapHeight=" + rawBitmapHeight);
+        int perPartWidth = rawBitmapWidth / column;
+        int perPartHeight = rawBitmapHeight / row;
+        System.out.println("perPartWidth=" + perPartWidth + ",perPartHeight=" + perPartHeight);
+        Bitmap bitmap;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                int x = j * perPartWidth;
+                int y = i * perPartHeight;
+                System.out.println("i=" + i + ",j=" + j + ",x=" + x + ",y=" + y);
+                bitmap = Bitmap.createBitmap(rawBitmap, x, y, perPartWidth, perPartHeight);
+                mSplitList.add(bitmap);
+            }
+        }
+        System.out.println("size=" + mSplitList.size());
+        return mSplitList;
+    }
 
 }
