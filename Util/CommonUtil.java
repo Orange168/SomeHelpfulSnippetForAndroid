6d9a34ebdd983f9ee72051c461c270ce861aa6da
Edward·D·Lin <zhilinchn@gmail.com>
2016-05-03 17:42:23 +0800

    add file md5 and bitmap md5

--- Util/CommonUtil.java
+++ Util/CommonUtil.java
@@ -57,4 +57,54 @@ public class CommonUtils {
         return mSplitList;
     }
 
+ /**
+     * 获取单个文件的MD5值
+     * @param file 文件
+     * @return 文件的MD5值
+     */
+    public static String getFileMD5(File file) {
+        if (!file.isFile()) {
+            return null;
+        }
+        MessageDigest digest;
+        FileInputStream in;
+        byte buffer[] = new byte[1024];
+        int len;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BigInteger bigInt = new BigInteger(1, digest.digest());
+        return bigInt.toString(16);
+    }
+    /**
+     * 获取单个文件的MD5值
+     * @param filePath 文件路径
+     * @return 文件的MD5值
+     */
+    public static String getFileMD5(String filePath) {
+//        Log.i("FileOperatorUtils", "getFileMD5" + getFileMD5(filePath));
+        return getFileMD5(new File(filePath));
+    }
+    
+       public static String getBitmapMD5(byte[] bytesBitmap) {
+        MessageDigest digest ;
+
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            digest.update(bytesBitmap);
+            BigInteger bigInt = new BigInteger(1, digest.digest());
+            return bigInt.toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
